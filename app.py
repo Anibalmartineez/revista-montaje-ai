ec0114ca847dcdef9a1348c0bcc9f27f2c51a470
Anibalmartineez <anibalmartineez98@gmail.com>
2025-06-28 11:45:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -93,5 +93,9 @@ def diagnosticar_pdf(path):
 """
     return diag
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
